ee8541899881843045b58580a395c58f1ea1115f
Jesse Preiner <jezzah@gmail.com>
2021-01-05 22:32:34 -0600

    Add an example using collect as well as reduce

--- src/main/java/com/jessepreiner/App.java
+++ src/main/java/com/jessepreiner/App.java
@@ -6,7 +6,8 @@ import java.util.List;
 import java.util.UUID;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.CompletionStage;
-import java.util.function.BinaryOperator;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class App {
     public static void main(String[] args) {
@@ -19,7 +20,7 @@ public class App {
                 getResponse(false),
                 getResponse(false));
 
-        HashMap<String, String> response = completionStages
+        Stream<Response> responses = completionStages
                 .stream()
                 .map(CompletionStage::toCompletableFuture)
                 .map(completableFuture ->
@@ -27,20 +28,43 @@ public class App {
                     CompletableFuture<Response> responseCompletableFuture = completableFuture.thenApply(y -> new SuccessResponse(new RequestView(UUID.randomUUID().toString())));
                     return responseCompletableFuture
                             .exceptionally((ex) -> new FailureResponse(new RequestView("failuuure"), ex.getMessage()));
-                }).map(CompletableFuture::join)
-                .reduce(new HashMap<>(), (a, b) -> {
-                    if (b instanceof SuccessResponse) {
-                        a.put("success", a.getOrDefault("success", "").concat(((SuccessResponse) b).view.toString()));
-                    } else {
-                        a.put("fail", a.getOrDefault("fail", "").concat(String.format("%s %s", ((FailureResponse) b).view.toString(), ((FailureResponse) b).failureMessage)));
+                }).map(CompletableFuture::join);
 
-                    }
-                    return a;
-                }, (a, b) -> new HashMap<>()); // i think this is needed because the accumulator type is different than stream item?
+        List<Response> responsesList = responses.collect(Collectors.toList());
+        HashMap<String, String> resultViaCollect = resultViaCollect(responsesList);
+        HashMap<String, String> resultViaReduce = resultViaReduce(responsesList);
 
-        System.out.println(response);
+        System.out.println(resultViaCollect);
+        System.out.println(resultViaReduce);
     }
 
+    private static HashMap<String, String> resultViaCollect(List<Response> responses) {
+        return responses.stream()
+                        .collect(HashMap::new, (a, b) -> {
+                            if (b instanceof SuccessResponse) {
+                                a.put("success", a.getOrDefault("success", "").concat(((SuccessResponse) b).view.toString()));
+                            } else {
+                                a.put("fail", a.getOrDefault("fail", "").concat(String.format("%s %s", ((FailureResponse) b).view.toString(), ((FailureResponse) b).failureMessage)));
+
+                            }
+                        }, HashMap::putAll);
+    }
+
+    private static HashMap<String, String> resultViaReduce(List<Response> responses) {
+        return responses.stream()
+                        .reduce(new HashMap<>(), (a, b) -> {
+                            if (b instanceof SuccessResponse) {
+                                a.put("success", a.getOrDefault("success", "").concat(((SuccessResponse) b).view.toString()));
+                            } else {
+                                a.put("fail", a.getOrDefault("fail", "").concat(String.format("%s %s", ((FailureResponse) b).view.toString(), ((FailureResponse) b).failureMessage)));
+                            }
+                            return a;
+                        }, (a, b) -> {
+                            a.putAll(b);
+                            return a;
+                        });
+
+    }
 
     private static CompletionStage<ResponseData> getResponse(boolean throwEx) {
         if (throwEx) {
